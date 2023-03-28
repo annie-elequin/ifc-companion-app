6416eede88c3e679b8276bce2ebbbb4073ed48ef
Jordan Smith <jordan.smiths.games@gmail.com>
2023-03-28 09:14:17 -0500

    Placeholder theme added for IFC colors later

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { theme } from "./src/screens/theme";
 
 import StartScreen from "./src/screens/StartScreen/StartScreen";
 import DemoOneScene from "./src/screens/DemoOne/DemoOne";
@@ -10,12 +13,14 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={StartScreen} />
-        <Stack.Screen name="DemoOneScene" component={DemoOneScene} />
-        <Stack.Screen name="DemoTwoScene" component={DemoTwoScene} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider theme={theme} >
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={StartScreen} />
+          <Stack.Screen name="DemoOneScene" component={DemoOneScene} />
+          <Stack.Screen name="DemoTwoScene" component={DemoTwoScene} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
