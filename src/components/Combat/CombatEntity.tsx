22ae06f0d8c21306d62ffcbd50f4200b81b74aa5
Jordan Smith <jordan.smiths.games@gmail.com>
2023-04-16 12:42:13 -0500

    Added block identifier

--- src/components/Combat/CombatEntity.tsx
+++ src/components/Combat/CombatEntity.tsx
@@ -1,4 +1,5 @@
-import { View, Image, AspectRatio } from "native-base";
+import { View, Image, Text, AspectRatio, VStack, Icon, HStack } from "native-base";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function CombatEntity(props) {
     const { name, health, image, altText, color } = props;
@@ -8,6 +9,15 @@ export default function CombatEntity(props) {
             <AspectRatio ratio={1}>
                 <Image w="100%" h="100%" source={{ uri: image }}></Image>
             </AspectRatio>
+            <VStack w="30%" h="100%" justifyContent="space-between">
+                <Text bold fontSize="4xl" color="#E01212"> - 8/14 +</Text>
+                <HStack>
+                    <HStack alignItems="center">
+                        <Icon as={<MaterialCommunityIcons name="shield" />} color="#333BFF" size="5xl"/>
+                        <Text bold fontSize="5xl" color="#000000">5</Text>
+                    </HStack>
+                </HStack>
+            </VStack>
         </View>
     )
 }
